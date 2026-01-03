3c358b79411567214cbb3e2c8ed090dd6005fcdf
Allen Suvorov <allen.suvorov@gmail.com>
2026-01-03 13:15:50 -0800

    check post request suggess with a get request to the created item by id

--- basics/unit-4/03_check_post_request_success.go
+++ basics/unit-4/03_check_post_request_success.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -36,13 +38,49 @@ func main() {
 		fmt.Println("New todo added successfully!")
 		fmt.Println(string(body))
 
-		// TODO: Perform a GET request
-
-		// resp2, err := http.Get(baseURL + "/todos/" + )
-		// TODO: Check if the GET request was successful and print the new todo item
-		// TODO: Handle potential errors from the GET request
+		// verifyAddition
+		id := getTodoId(body)
+		verifyAddition(id, baseURL)
 	} else {
 		// Handle potential errors from the POST request
 		fmt.Printf("Failed to add a new todo\nStatus Code: %d\nError Details: %s\n", resp.StatusCode, string(body))
 	}
+}
+
+func getTodoId(body []byte) string {
+	type createTodoResponse struct {
+		ID int `json: "id"` // why did not use these tags? why was it seen as a number by unmarshal while we are parsing a string?
+	}
+
+	var resp createTodoResponse
+	err := json.Unmarshal(body, &resp)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	return strconv.Itoa(resp.ID)
+}
+
+/*
+ */
+
+func verifyAddition(id, baseURL string) {
+	// TODO: Perform a GET request
+	resp, err := http.Get(baseURL + "/todos/" + id)
+	// TODO: Handle potential errors from the GET request
+	if err != nil {
+		log.Fatalf("Could not get a todo, error: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+	// TODO: Check if the GET request was successful and print the new todo item
+	if resp.StatusCode == http.StatusOK {
+		fmt.Printf("Varified new todo item created: %s", body)
+	} else {
+		fmt.Printf("Unexpected Status Code: %d \n", resp.StatusCode)
+		fmt.Printf("Error Details: %s\n", body)
+	}
+
 }
